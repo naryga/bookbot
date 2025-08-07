1ceb166ede60ba99dca94c9999c60e3261e23a0f
Nathan Garza <nathan@5onfire.com>
2025-08-07 14:35:56 -0600

    add args, usage reminders, pretty reporting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sys
 from stats import get_num_words
 from stats import count_char_instances
+from stats import formated_chars
 
 def get_book_text(path_to_file):
     
@@ -9,11 +11,29 @@ def get_book_text(path_to_file):
     
     return file_contents
 
+def pretty_report(book_file, num_words, num_chars):
+    # formated report
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_file}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char in num_chars:
+        if char["char"].isalpha():
+            print(f"{char["char"]}: {char["num"]}")
+
 def main():
-    txt = get_book_text('./books/frankenstein.txt')
+    # must have at least 2 arguments
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_file = sys.argv[1]
+    txt = get_book_text(book_file)
     num_words = get_num_words(txt)
     num_chars = count_char_instances(txt)
-    print(f"{num_words} words found in the document")
-    print(num_chars)
+    num_chars = formated_chars(num_chars)
+
+    pretty_report(book_file, num_words, num_chars)
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,15 @@ def count_char_instances(str_of_chars):
             char_counts[char] += 1
     
     return char_counts
+
+def sort_on(items):
+    return items["num"]
+
+def formated_chars(char_dict):
+    pretty_char_list = []
+    for char in char_dict:
+        pretty_char_list.append({"char": char, "num": char_dict[char]})
+
+    pretty_char_list.sort(reverse=True, key=sort_on)
+
+    return pretty_char_list
